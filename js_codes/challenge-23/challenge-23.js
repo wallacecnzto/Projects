a07ec761063c97363a3bea68da4389d5d976f0d2
wallacecnzto <wallacewvinfo@gmail.com>
2017-10-21 21:00:40 -0200

    Add challenge 23

--- js_codes/challenge-23/challenge-23.js
+++ js_codes/challenge-23/challenge-23.js
@@ -3,18 +3,56 @@
 
     var $visor = doc.querySelector('[data-js="visor"]');
     var $buttonsNumbers = doc.querySelectorAll('[data-js="button-number"]');
+    var $buttonsOperations = doc.querySelectorAll('[data-js="button-operation"]');
+    var $buttonCE = doc.querySelector('[data-js="button-ce"]');
+    var $buttonEqual = doc.querySelector('[data-js="button-equal"]');
 
     Array.prototype.forEach.call($buttonsNumbers, function(button) {
         button.addEventListener('click', handleClickNumber, false);
     });
 
-    function handleClickNumber(event) {
+    Array.prototype.forEach.call($buttonsOperations, function(button) {
+        button.addEventListener('click', handleClickOperation, false);
+    });
+
+   $buttonCE.addEventListener('click', handleClickCE, false);
+   $buttonEqual.addEventListener('click', handleClickEqual, false);
+
+    function handleClickNumber() {
         $visor.value += this.value;
     }
 
+    function handleClickOperation() {
+        removeLastItemIfItIsAnOperator();
+        $visor.value += this.value;
+    }
 
+    function handleClickCE() {
+        $visor.value = 0;
+    }
 
+    function isLastItemAnOperation() {
+        var operations = ['+', '-', '*', '/'];
+        var lastItem = $visor.value.split('').pop();
+        return operations.some(function(operator) {
+            return operator === lastItem;
+        });
+    }
 
+    function removeLastItemIfItIsAnOperator() {
+        if(isLastItemAnOperation())
+            $visor.value = $visor.value.slice(0, -1);
+    }
+
+    function handleClickEqual() {
+        removeLastItemIfItIsAnOperator();
+        var allValues = $visor.value.match(/\d+[+x\/-]?/g);
+        var result = allValues.reduce(function(accumulated, actual) {
+            return accumulated + actual;
+        });
+
+        console.log(result);
+    }
 
 
 
