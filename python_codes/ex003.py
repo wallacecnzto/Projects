14fb6f226d363b1b0b6ef37f90897b4b967ec262
wallacecnzto <wallacewvinfo@gmail.com>
2017-11-07 17:33:29 -0200

    Add lesson 003.py

--- python_codes/ex003.py
+++ python_codes/ex003.py
@@ -1,4 +1,8 @@
-first_number = input('Digite o primeiro número: ')
-second_number = input('Digite o segundo número: ')
+first_number = int(input('Digite o primeiro número: '))
+second_number = int(input('Digite o segundo número: '))
+sum = first_number + second_number
 
-print(int(first_number) + int(second_number))
+print('A soma de {} + {} é igual a: {}'.format(
+    first_number,
+    second_number,
+    sum))
