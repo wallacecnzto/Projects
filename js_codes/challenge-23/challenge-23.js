c5bbb75e7b5d213d77c9d5e7657d2ac4a6646bd1
wallacecnzto <wallacewvinfo@gmail.com>
2017-11-11 02:58:34 -0200

    Ending challenge-23.js

--- js_codes/challenge-23/challenge-23.js
+++ js_codes/challenge-23/challenge-23.js
@@ -23,7 +23,7 @@
     }
 
     function handleClickOperation() {
-        removeLastItemIfItIsAnOperator();
+        $visor.value = removeLastItemIfItIsAnOperator($visor.value);
         $visor.value += this.value;
     }
 
@@ -31,43 +31,42 @@
         $visor.value = 0;
     }
 
-    function isLastItemAnOperation() {
+    function isLastItemAnOperation(number) {
         var operations = ['+', '-', '*', '/'];
-        var lastItem = $visor.value.split('').pop();
+        var lastItem = number.split('').pop();
         return operations.some(function(operator) {
             return operator === lastItem;
         });
     }
 
-    function removeLastItemIfItIsAnOperator() {
-        if(isLastItemAnOperation())
-            $visor.value = $visor.value.slice(0, -1);
+    function removeLastItemIfItIsAnOperator(number) {
+        if(isLastItemAnOperation(number)) {
+            return number.slice(0, -1);
+        }
+
+        return number;
     }
 
     function handleClickEqual() {
-        removeLastItemIfItIsAnOperator();
+        $visor.value = removeLastItemIfItIsAnOperator($visor.value);
         var allValues = $visor.value.match(/\d+[+x\/-]?/g);
-        var result = allValues.reduce(function(accumulated, actual) {
-            return accumulated + actual;
+        $visor.value = allValues.reduce(function(accumulated, actual) {
+            var firstValue = accumulated.slice(0, -1);
+            var operator = accumulated.split('').pop();
+            var lastValue = removeLastItemIfItIsAnOperator(actual);
+            var lastOperator = isLastItemAnOperation(actual) ? actual.split('').pop() : '';
+            switch(operator) {
+                case '+':
+                    return ( Number(firstValue) + Number(lastValue) ) + lastOperator;
+                case '-':
+                    return ( Number(firstValue) - Number(lastValue) ) + lastOperator;
+                case '*':
+                    return ( Number(firstValue) * Number(lastValue) ) + lastOperator;
+                case '/':
+                    return ( Number(firstValue) / Number(lastValue) ) + lastOperator;
+            }
         });
 
-        console.log(result);
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 })(window, document);
