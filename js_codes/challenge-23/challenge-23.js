8001e73e42fefcb69ac8f14f002ceedf1fdcddf0
wallacecnzto <wallacewvinfo@gmail.com>
2017-11-15 20:04:53 -0200

    Refatoring challenge-23.js

--- js_codes/challenge-23/challenge-23.js
+++ js_codes/challenge-23/challenge-23.js
@@ -7,16 +7,22 @@
     var $buttonCE = doc.querySelector('[data-js="button-ce"]');
     var $buttonEqual = doc.querySelector('[data-js="button-equal"]');
 
-    Array.prototype.forEach.call($buttonsNumbers, function(button) {
-        button.addEventListener('click', handleClickNumber, false);
-    });
+    function initialize() {
+        initEvents();
+    }
 
-    Array.prototype.forEach.call($buttonsOperations, function(button) {
-        button.addEventListener('click', handleClickOperation, false);
-    });
+    function initEvents() {
+        Array.prototype.forEach.call($buttonsNumbers, function(button) {
+            button.addEventListener('click', handleClickNumber, false);
+        });
 
-   $buttonCE.addEventListener('click', handleClickCE, false);
-   $buttonEqual.addEventListener('click', handleClickEqual, false);
+        Array.prototype.forEach.call($buttonsOperations, function(button) {
+            button.addEventListener('click', handleClickOperation, false);
+        });
+
+        $buttonCE.addEventListener('click', handleClickCE, false);
+        $buttonEqual.addEventListener('click', handleClickEqual, false);
+    }
 
     function handleClickNumber() {
         $visor.value += this.value;
@@ -32,41 +38,58 @@
     }
 
     function isLastItemAnOperation(number) {
-        var operations = ['+', '-', '*', '/'];
+        var operations = getOperations();
         var lastItem = number.split('').pop();
         return operations.some(function(operator) {
             return operator === lastItem;
         });
     }
 
-    function removeLastItemIfItIsAnOperator(number) {
-        if(isLastItemAnOperation(number)) {
-            return number.slice(0, -1);
+    function getOperations() {
+        return Array.prototype.map.call($buttonsOperations, function(button) {
+            return button.value;
+        });
+    }
+
+    function removeLastItemIfItIsAnOperator(string) {
+        if(isLastItemAnOperation(string)) {
+            return string.slice(0, -1);
         }
 
-        return number;
+        return string;
     }
 
     function handleClickEqual() {
         $visor.value = removeLastItemIfItIsAnOperator($visor.value);
-        var allValues = $visor.value.match(/\d+[+x\/-]?/g);
-        $visor.value = allValues.reduce(function(accumulated, actual) {
-            var firstValue = accumulated.slice(0, -1);
-            var operator = accumulated.split('').pop();
-            var lastValue = removeLastItemIfItIsAnOperator(actual);
-            var lastOperator = isLastItemAnOperation(actual) ? actual.split('').pop() : '';
-            switch(operator) {
-                case '+':
-                    return ( Number(firstValue) + Number(lastValue) ) + lastOperator;
-                case '-':
-                    return ( Number(firstValue) - Number(lastValue) ) + lastOperator;
-                case '*':
-                    return ( Number(firstValue) * Number(lastValue) ) + lastOperator;
-                case '/':
-                    return ( Number(firstValue) / Number(lastValue) ) + lastOperator;
-            }
-        });
+        var allValues = $visor.value.match(getRegexOperations());
+        $visor.value = allValues.reduce(calculateAllValues);
 
     }
 
+    function getRegexOperations() {
+        return new RegExp('\\d+[' + getOperations().join('') + ']?', 'g');
+    }
+
+    function calculateAllValues(accumulated, actual) {
+        var firstValue = accumulated.slice(0, -1);
+        var operator = accumulated.split('').pop();
+        var lastValue = removeLastItemIfItIsAnOperator(actual);
+        var lastOperator = isLastItemAnOperation(actual) ? actual.split('').pop() : '';
+        return doOperation(operator, firstValue, lastValue) + lastOperator;
+    }
+
+    function doOperation(operator, firstValue, lastValue) {
+        switch(operator) {
+            case '+':
+                return Number(firstValue) + Number(lastValue);
+            case '-':
+                return Number(firstValue) - Number(lastValue);
+            case '*':
+                return Number(firstValue) * Number(lastValue);
+            case '/':
+                return Number(firstValue) / Number(lastValue);
+        }
+    }
+
+    initialize();
 })(window, document);
