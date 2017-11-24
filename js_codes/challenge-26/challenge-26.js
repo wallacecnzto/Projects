5178370ecdab10f0d2b039978ecd8ae2e255c561
wallacecnzto <wallacewvinfo@gmail.com>
2017-11-24 21:24:05 -0200

    Ended challenge 26

--- js_codes/challenge-26/challenge-26.js
+++ js_codes/challenge-26/challenge-26.js
@@ -2,44 +2,33 @@
     'use strict';
 
     function DOM(elements) {
-        this.element = this.getDOMElements(elements);
+        this.element = document.querySelectorAll(elements);
     }
 
-    DOM.prototype.getDOMElements = function getDOMElements(elements) {
-        return document.querySelectorAll(elements);
+    DOM.prototype.on = function on(eventType, callback) {
+        Array.prototype.forEach.call(this.element, function(element) {
+            element.addEventListener(eventType, callback, false);
+        });
     };
 
-    DOM.prototype.on = function on() {};
-    DOM.prototype.off = function off() {};
-    DOM.prototype.get = function get() {};
+    DOM.prototype.off = function off(eventType, callback) {
+        Array.prototype.forEach.call(this.element, function(element) {
+            element.removeEventListener(eventType, callback, false);
+        });
+    };
+
+    DOM.prototype.get = function get() {
+        return this.element;
+    };
 
     var $a = new DOM ('[data-js="link"]');
-    $a.on('click', function(e) {
+    $a.on('click', function handleClick(e) {
         e.preventDefault();
         console.log('clicou');
+        $a.off('click', handleClick);
     });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    console.log('Elementos selecionados:', $a.get());
+    console.log('$a é filho de body?', $a.get()[0].parentNode === document.body);
 
 })();
