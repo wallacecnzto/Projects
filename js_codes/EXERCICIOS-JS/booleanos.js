4642fd557ee96c3f94815a30e9cca0720efd8a97
wallacecnzto <wallacewvinfo@gmail.com>
2018-01-22 22:53:05 -0200

    Add paste of Cod3rs course

---
+++ js_codes/EXERCICIOS-JS/booleanos.js
@@ -0,0 +1,11 @@
+// valores falsos (!! retorna o booleano da expressão)
+
+console.log(!!0)
+console.log(!!'')
+console.log(!!null)
+console.log(!!NaN)
+console.log(!!undefined)
+
+let nome = ''
+
+console.log( nome || 'Desconhecido')
