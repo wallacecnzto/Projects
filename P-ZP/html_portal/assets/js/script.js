872ce5819ace68b52a64448053208542c67bfb4c
wallacecnzto <wallacewvinfo@gmail.com>
2018-06-11 21:05:49 -0300

    Ending slideshow and noticiaarea.

--- P-ZP/html_portal/assets/js/script.js
+++ P-ZP/html_portal/assets/js/script.js
@@ -1,7 +1,28 @@
+var slideItem = 0;
 window.onload = function() {
+    setInterval(passarSlide, 3000);
+
     var slidewidth = this.document.getElementById("slideshow").offsetWidth;
     var objs = this.document.getElementsByClassName("slide");
+
     for(i=0;i<objs.length;i++) {
         objs[i].style.width = slidewidth + 'px';
     }
+}
+function passarSlide() {
+    var slidewidth = document.getElementById("slideshow").offsetWidth;
+
+    if (slideItem >= 3) {
+        slideItem = 0;
+    }else {
+        slideItem++;
+    }
+
+    document.getElementsByClassName("slideshowarea")[0].style.marginLeft = "-"+(slidewidth * slideItem)+"px";
+}
+function mudarSlide(pos) {
+    slideItem = pos;
+    var slidewidth = document.getElementById("slideshow").offsetWidth;
+    document.getElementsByClassName("slideshowarea")[0].style.marginLeft = "-"+(slidewidth * slideItem)+"px";
+
 }
