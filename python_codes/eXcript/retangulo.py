7cd12c635a085a2b9aae7775dfe310fa45fa6618
wallacecnzto <wallacewvinfo@gmail.com>
2019-10-12 16:46:48 -0300

    Changed retangulo.py

--- python_codes/eXcript/retangulo.py
+++ python_codes/eXcript/retangulo.py
@@ -11,7 +11,7 @@ class Retangulo:
         self._altura = num
 
     def set_largura(self, num):
-        if(not(isinstance(num, int)and(num > 0))):
+        if not(isinstance(num, int) and (num > 0)):
             raise ValueError("A largura é inválida: {}".format(num))
         self._largura = num
 
