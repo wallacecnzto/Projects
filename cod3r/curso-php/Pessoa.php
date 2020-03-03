9320c59a7864df9de691b98f61b1fb1434cda066
wallacecnzto <wallacewvinfo@gmail.com>
2020-03-03 17:57:32 -0300

    Create example OO in PHP

---
+++ cod3r/curso-php/Pessoa.php
@@ -0,0 +1,46 @@
+<?php
+
+class Pessoa {
+
+    public $nome;
+    public $idade;
+
+    function __construct($nome, $idade) {
+        $this->nome = $nome;
+        $this->idade = $idade;
+        echo 'Pessoa criada! <br>'; 
+    }
+
+    function __destruct(){
+        echo 'Tchau!';
+    }
+
+    public function apresentar() {
+        echo "{$this->nome}, {$this->idade} anos <br>";
+    }
+}
+
+class Usuario extends Pessoa {
+    
+    public $login;
+
+    function __construct($nome, $idade, $login) {
+        $this->nome = $nome;
+        $this->idade = $idade;
+        $this->login = $login;
+        echo 'Usuário logado!';
+    }
+
+    function __destruct() { 
+        echo 'Usuário diz TCHAU!';
+    }
+
+    public function apresentar() {
+        echo "@{$this->login}!!!";
+        parent::apresentar(); //parent invoca comportamentos
+    }
+
+}
+
+$usuario = new Usuario('Wallace', 42, 'cnzto');
+$usuario->apresentar();
